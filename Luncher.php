45a2892c6d4a2fb29bb8d582a415a375253c8716
Arsalan <hacker4moon@gmail.com>
2016-06-30 23:17:42 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '227129618:AAExK1BrBfBjGYEPEG6gDbiaUA7U0bK3kXg');
+define('BOT_TOKEN', '227129618:AAExK1BrBfBjGYEPEG');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
